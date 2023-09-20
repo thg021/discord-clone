69907f3c623b2defc55b80532862a3d83d05bc84
thiago da silva <thg021@gmail.com>
2023-09-19 22:17:03 -0300

    feat: sidebar

--- hooks/use-model-store.ts
+++ hooks/use-model-store.ts
@@ -1,11 +1,20 @@
+import { Channel, ChannelType, Server } from '@prisma/client';
 import { create } from 'zustand'
 
-export type ModalType = "createServer"
+export type ModalType = "createServer" | 'invite' | 'editServer' | 'members' | 'createChannel' | 'deleteServer' | 'leaveServer'
+
+interface ModalData {
+  server?: Server;
+  channel?: Channel;
+  channelType?: ChannelType;
+  apiUrl?: string;
+  query?: Record<string, any>;
+}
 
 interface ModalStore {
     type: ModalType | null
     isOpen: boolean
-    onOpen: (type: ModalType) => void 
+    onOpen: (type: ModalType, data?: ModalData) => void 
     onClose: () => void 
 }
 
